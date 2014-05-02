a6cf367edf75c7223df152fd9e4adc6820a087ee
claudiu <claudiu.garba@gmail.com>
2014-05-02 22:26:58 +0200

    again

--- spec/lib/zombie.rb
+++ spec/lib/zombie.rb
@@ -8,4 +8,20 @@ class Zombie
   def hungry?
     true
   end
+
+  def weapons
+    2
+  end
+
+  def eat(type_of_food)
+    if type_of_food == "brain"
+      "yummmy"
+    else
+      "bleahhhh"
+    end
+  end
+
+  def color
+    'green'
+  end
 end
--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -8,6 +8,26 @@ describe Zombie do
   end
 
   it "should be houngry" do
-    expect(Zombie.new.hungry?).to be_true
+    expect(Zombie.new.hungry?).to eq true
+  end
+
+  it "is invalid with out a name" do
+    expect(Zombie.new).not_to be_nil
+  end
+
+  it "should have 2 weapons" do
+    expect(Zombie.new.weapons).to eq 2
+  end
+
+  it "should eat only brains" do
+    zombie = Zombie.new
+    expect(zombie.eat("brain")).to eq "yummmy"
+    expect(zombie.eat("bread")).to eq "bleahhhh"
+  end
+
+  it 'should have color blue or green' do
+    zombie = Zombie.new
+    expect(zombie.color).to eql 'green' || 'blue'
+   # expect(zombie.color).to eql 'blue'
   end
 end
